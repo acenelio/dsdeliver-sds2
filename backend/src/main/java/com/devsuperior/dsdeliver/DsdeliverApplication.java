2c2f6c41d0ad6845f8a7da837e4dc91bf5e55f73
Nelio Alves <acenelio@gmail.com>
2021-01-02 11:25:37 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
